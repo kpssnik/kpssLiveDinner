84d0ea13e30663ef497e9961c2ab1f8e7c9e927f
kpssnik <kpssdeveloper@gmail.com>
2021-02-02 12:03:28 +0200

    Created empty MVC project

---
+++ kpssLiveDinner/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace kpssLiveDinner.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
