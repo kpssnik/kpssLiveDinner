6d6b5f947efe5aa30fc21a7d7642b6c45dbde906
kpssnik <kpssdeveloper@gmail.com>
2021-02-02 19:41:04 +0200

    Added all pages with the ability to integrate models

--- kpssLiveDinner/Controllers/HomeController.cs
+++ kpssLiveDinner/Controllers/HomeController.cs
@@ -22,11 +22,31 @@ namespace kpssLiveDinner.Controllers
         {
             return View();
         }
-
         public IActionResult About()
         {
             return View();
+        }  
+        public IActionResult Blog()
+        {
+            return View();
         }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Reservation()
+        {
+            return View();
+        }
+        public IActionResult Stuff()
+        {
+            return View();
+        }
+        public IActionResult Gallery()
+        {
+            return View();
+        }
+
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
